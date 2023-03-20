52d895af0307f752c909742c769b7a8b589cac75
Wanderleyps <wanderleyps794@gmail.com>
2023-03-20 17:40:52 -0300

    update

---
+++ Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using AcademiaWeb.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcademiaWeb.Data.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        //Mapeamento
+       // public DbSet<Category> Categories { get; set; }
+        //public DbSet<Product> Products { get; set; }
+
+        //método permite configurar o modelo usando a Fluent API
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //utlizando para refenciar automaticamente todas as entidades criadas nas classes de EntitiesConfigurations
+            //desse forma não precisa instanciar um por um (ex. builder.ApplyConfigurations(new ProductConfiguation();)
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
